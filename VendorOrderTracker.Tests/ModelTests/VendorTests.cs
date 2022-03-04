6c0f72297bad8574d94bcbb0d60a43a7fca3f876
Will W <wjwat@onionslice.org>
2022-03-04 12:21:05 -0600

    Build out code for testing Vendor class

---
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    // Run some tests
+  }
+}
