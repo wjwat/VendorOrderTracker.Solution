fb58f08c6be478484230861ebd59dc12a5054c49
Will W <wjwat@onionslice.org>
2022-03-06 08:00:25 -0600

    Add ability to edit order values

--- VendorOrderTracker/Controllers/OrdersController.cs
+++ VendorOrderTracker/Controllers/OrdersController.cs
@@ -24,9 +24,45 @@ namespace VendorOrderTracker.Controllers
       Dictionary<string, object> model = new Dictionary<string, object> {
         { "vendor", vendor },
         { "order", order },
+        { "orderId", orderId },
       };
 
       return View(model);
+    }
+
+    // This is pretty similar to route above, how would I DRY this?
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}/edit")]
+    public ActionResult Edit(int vendorId, int orderId)
+    {
+      Vendor vendor = Vendor.FindVendor(vendorId);
+      Order order = vendor.Orders[orderId];
+
+      Dictionary<string, object> model = new Dictionary<string, object> {
+        { "vendor", vendor },
+        { "order", order },
+        { "orderId", orderId },
+      };
+
+      return View(model);
+    }
+
+    [HttpPost("/vendors/{vendorId}/orders/{orderId}/edit")]
+    public ActionResult Edit(int vendorId,
+        int orderId,
+        int id,
+        string title,
+        string description,
+        int price,
+        string date)
+    {
+      Order order = Vendor.FindVendor(vendorId).Orders[orderId];
+
+      order.Title = title;
+      order.Description = description;
+      order.Price = price;
+      order.Date = date;
+
+      return RedirectToAction("Show", new { orderId, vendorId });
     }
   }
 }
--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -4,10 +4,10 @@ namespace VendorOrderTracker.Models
 {
   public class Order
   {
-    public string Title { get; init; }
-    public string Description { get; init; }
-    public int Price { get; init; }
-    public string Date { get; init; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Date { get; set; }
 
     public Order(string title, string description, int price, string date)
     {
